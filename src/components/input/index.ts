755eb85ef00aa8e55c2e3f23db4132b389a3f9ec
Ihar-Batura <A17331582@gmail.com>
2024-11-25 14:56:12 +0300

    fix: fix input component

---
+++ src/components/input/index.ts
@@ -0,0 +1,47 @@
+import styles from './index.module.css';
+import { InputProps } from './types';
+
+function Input({
+  type,
+  id,
+  className,
+  title,
+  required,
+  placeholder,
+  minLength,
+  pattern,
+  parent,
+}: InputProps): HTMLInputElement {
+  const input = document.createElement('input');
+
+  input.setAttribute('type', type);
+  if (id) {
+    input.setAttribute('id', id);
+  }
+  input.classList.add(styles.input);
+  if (className) {
+    input.classList.add(className);
+  }
+  if (title) {
+    input.setAttribute('title', title);
+  }
+  if (required) {
+    input.setAttribute('required', '');
+  }
+  if (placeholder) {
+    input.setAttribute('placeholder', placeholder);
+  }
+  if (minLength) {
+    input.setAttribute('minlength', minLength);
+  }
+  if (pattern) {
+    input.setAttribute('pattern', pattern);
+  }
+  if (parent) {
+    parent.append(input);
+  }
+
+  return input;
+}
+
+export default Input;
---
+++ src/components/input/types.ts
@@ -0,0 +1,11 @@
+export type InputProps = {
+  type: string;
+  id?: string;
+  className?: string;
+  title?: string;
+  required?: string;
+  placeholder?: string;
+  minLength?: string;
+  pattern?: string;
+  parent?: HTMLElement;
+};
