cf9abd32d13406ee46abd600f3c148b57f0678d7
Ihar-Batura <A17331582@gmail.com>
2024-11-25 18:30:56 +0300

    feat: add span component

---
+++ src/components/span/index.ts
@@ -0,0 +1,24 @@
+import styles from './index.module.css';
+import { SpanProps } from './types';
+
+function Span({ id, className, text, parent }: SpanProps): HTMLSpanElement {
+  const span = document.createElement('span');
+
+  if (id) {
+    span.setAttribute('id', id);
+  }
+  span.classList.add(styles.span);
+  if (className) {
+    span.classList.add(className);
+  }
+  if (text) {
+    span.textContent = text;
+  }
+  if (parent) {
+    parent.append(span);
+  }
+
+  return span;
+}
+
+export default Span;
---
+++ src/components/span/types.ts
@@ -0,0 +1,6 @@
+export type SpanProps = {
+  id?: string;
+  className?: string;
+  text?: string;
+  parent?: HTMLElement;
+};
