76dc4f5aca6afa0168ac2ead7015e256226329ce
Ihar-Batura <A17331582@gmail.com>
2024-12-04 10:39:52 +0300

    feat: add function whatRoundChoose

---
+++ src/functional/what_round.ts
@@ -0,0 +1,13 @@
+function whatRoundChoose(): string {
+  const roundBtn: HTMLSelectElement | null =
+    document.querySelector('#round-btn');
+  if (roundBtn) {
+    const round = roundBtn.value;
+    //console.log(level);
+    return round;
+  } else {
+    return '1';
+  }
+}
+
+export default whatRoundChoose;
