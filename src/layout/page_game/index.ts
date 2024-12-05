49b0fb9602562ee85a96d25713af203ba6d196f6
Ihar-Batura <A17331582@gmail.com>
2024-12-05 10:48:13 +0300

    feat: add text hint element

--- src/layout/page_game/index.ts
+++ src/layout/page_game/index.ts
@@ -1,16 +1,18 @@
 import cleanMainContainer from '../../functional/clean_main_container';
 import createHintsContainer from '../game_hints';
-//import whatLevelChoosed from '../../functional/what_level';
+import createDiv from '../../components/div';
 
 function createPageGame() {
   cleanMainContainer();
   const main: HTMLElement | null = document.querySelector('main');
   if (main) {
     const hintsContainer = createHintsContainer();
+    const textHint = createDiv({
+      className: 'text-hint',
+      text: 'Здесь будет подсказка',
+    });
 
-    main.append(hintsContainer);
-
-    // whatLevelChoosed();
+    main.append(hintsContainer, textHint);
   }
 }
 
