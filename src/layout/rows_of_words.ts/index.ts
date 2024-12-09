ca33a57c641c7be2daa2c30b4f4be182412c0a5b
Ihar-Batura <A17331582@gmail.com>
2024-12-09 18:22:35 +0300

    fix: fix function createWordRows

--- src/layout/rows_of_words.ts/index.ts
+++ src/layout/rows_of_words.ts/index.ts
@@ -2,11 +2,23 @@ import createDiv from '../../components/div';
 import { Iwords } from '../../functional/start_round';
 
 function createWordRows(words: Iwords[]) {
-  const gameFild: HTMLDivElement | null = document.querySelector('.game-board');
-  if (gameFild) {
-    gameFild.innerHTML = '';
+  const gameField: HTMLDivElement | null =
+    document.querySelector('.game-board');
+  if (gameField) {
+    gameField.innerHTML = '';
     for (let i = 0; i < words.length; i += 1) {
-      createDiv({ className: 'game-board__row', parent: gameFild });
+      const row: HTMLDivElement = createDiv({ className: 'game-board__row' });
+      const arrRowWords = words[i].textExample.split(' ');
+
+      for (let j = 0; j < arrRowWords.length; j += 1) {
+        createDiv({
+          className: 'game-board__row-word',
+          text: `${arrRowWords[j]}`,
+          parent: row,
+        });
+      }
+      console.log(arrRowWords);
+      gameField.append(row);
     }
   }
 }
