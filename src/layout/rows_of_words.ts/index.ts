4b210adf3d5de3dea6e1ef174a9263e266996351
Ihar-Batura <A17331582@gmail.com>
2024-12-10 10:54:45 +0300

    feat: add dynamic word card sizing

--- src/layout/rows_of_words.ts/index.ts
+++ src/layout/rows_of_words.ts/index.ts
@@ -6,16 +6,24 @@ function createWordRows(words: Iwords[]) {
     document.querySelector('.game-board');
   if (gameField) {
     gameField.innerHTML = '';
+    const rowLength: number = gameField.offsetWidth; // определяем ширину игрового поля
     for (let i = 0; i < words.length; i += 1) {
       const row: HTMLDivElement = createDiv({ className: 'game-board__row' });
-      const arrRowWords = words[i].textExample.split(' ');
+
+      const numberOfLetters: number = words[i].textExample.length; // получаем количество символов в предложение
+      const arrRowWords = words[i].textExample.split(' '); // разбиваем предложение на массив и  можем узнать колличество пробелов
+      const letterWidth: number =
+        rowLength / (numberOfLetters - arrRowWords.length - 1); // получаем чистую ширину одного символа
 
       for (let j = 0; j < arrRowWords.length; j += 1) {
-        createDiv({
+        const rowItem = createDiv({
           className: 'game-board__row-word',
           text: `${arrRowWords[j]}`,
-          parent: row,
         });
+        const rowItemWidth: number = letterWidth * arrRowWords[j].length; // ширина одного блока с словом
+        rowItem.style.width = `${rowItemWidth}px`;
+
+        row.append(rowItem);
       }
       console.log(arrRowWords);
       gameField.append(row);
